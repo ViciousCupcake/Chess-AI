be42d42b230f1fa175bd79ed59323c3b51aea535
ViciousCupcake <22625405+ViciousCupcake@users.noreply.github.com>
2020-12-21 18:59:08 -0800

    test helper function in minimax

--- src/helpers/minimax.js
+++ src/helpers/minimax.js
@@ -3,4 +3,8 @@ export default function minimax(a, b, self) {
     console.log(b);
     self.handleClick(8);
     self.handleClick(16);
+    //helloWorld();
+}
+function evaluateScore(squares, currentColor) {
+    console.log("Hello World");
 }
