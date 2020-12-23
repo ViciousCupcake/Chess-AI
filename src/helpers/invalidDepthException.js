20a764fa2f22e8b0135ad6de9aef3d6f19f2d38a
ViciousCupcake <22625405+ViciousCupcake@users.noreply.github.com>
2020-12-23 00:25:47 -0800

    Add exception for when depth is <= 1

---
+++ src/helpers/invalidDepthException.js
@@ -0,0 +1,12 @@
+export default function InvalidDepthException() {
+    this.message = "Invalid Depth was Provided! Depth must be >= 1";
+    // Use V8's native method if available, otherwise fallback
+    if ("captureStackTrace" in Error)
+        Error.captureStackTrace(this, InvalidDepthException);
+    else
+        this.stack = (new Error()).stack;
+}
+
+InvalidDepthException.prototype = Object.create(Error.prototype);
+InvalidDepthException.prototype.name = "InvalidDepthException";
+InvalidDepthException.prototype.constructor = InvalidDepthException;
